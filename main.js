b9c55e41865166d509a56d9b8a431083d5cf1cce
Stanislav Fedyna <33604153+iStanislav07@users.noreply.github.com>
2018-01-10 16:11:51 -0800

    added cycle

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ var name = "Rafael";
 var time = "09:00";
 var place = "Lviv";
 var event = prompt("What kind of meeting?");
-	
-
+event = event.replace(/\d/g, '');
 
 while(event >= -Infinity && event < Infinity){
+	
 	console.log("Ops,problem, try write another word or phrases");
 	event = prompt("What kind of meeting?");
 	
@@ -19,17 +19,24 @@ alert(name + " has a " + event + " today at " + time + " in " + place);
 //TASK 2
 var euro = 32.6;
 var dollar = 27.2;
-
-
- var itemEuro;
- do {
-   itemEuro = prompt("Enter ammount of EUR");
- } while (itemEuro.trim() === "" || isNaN(itemEuro)); 
+var itemEuro;
+//Додати цикл перевірки на довжину числа
+	itemEuro = prompt("Enter ammount of EUR");
+ 	
+	for (itemEuro; itemEuro.length >= 8; itemEuro = prompt("Enter ammount of EUR"))
+	  {
+		alert("Ops, try please again");
+	  }
+		 itemEuro.trim() === "" || isNaN(itemEuro);
 
  var itemDollar;
- do {
-   itemDollar = prompt("Enter ammount of Dollar");
- } while (itemDollar.trim() === "" || isNaN(itemDollar)); 
+	itemDollar = prompt("Enter ammount of Dollar");
+ 	
+	for (itemDollar; itemDollar.length >= 8; itemDollar = prompt("Enter ammount of Dollar"))
+	  {
+		alert("Ops, try please again");
+	  }
+		 itemDollar.trim() === "" || isNaN(itemDollar);
  
 
 
