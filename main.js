00449c6a8e48076f61931f29ba76638ce76c185f
Stanislav Fedyna <33604153+iStanislav07@users.noreply.github.com>
2018-01-15 13:28:00 -0800

    added symbol test

--- main.js
+++ main.js
@@ -2,14 +2,13 @@
 var name = "Rafael";
 var time = "09:00";
 var place = "Lviv";
-var event = prompt("What kind of meeting?");
+var event = prompt("What kind of meeting?","Dinner");
 event = event.replace(/\d/g, '');
 
 while(event >= -Infinity && event < Infinity){
 	
 	console.log("Ops,problem, try write another word or phrases");
 	event = prompt("What kind of meeting?");
-	
 }
 	
 console.log(name + " has a " + event + " today at " + time + " somwhere in " + place);
@@ -20,24 +19,33 @@ alert(name + " has a " + event + " today at " + time + " in " + place);
 var euro = 32.6;
 var dollar = 27.2;
 var itemEuro;
-//Додати цикл перевірки на довжину числа
-	itemEuro = prompt("Enter ammount of EUR");
- 	
-	for (itemEuro; itemEuro.length >= 8; itemEuro = prompt("Enter ammount of EUR"))
-	  {
-		alert("Ops, try please again");
-	  }
-		 itemEuro.trim() === "" || isNaN(itemEuro);
 
- var itemDollar;
-	itemDollar = prompt("Enter ammount of Dollar");
+	itemEuro = prompt("Enter ammount of EUR", "100");
+ 	//Added lengh-cycle in form
+
+		
+	for (itemEuro; itemEuro.length >= 10; itemEuro = prompt("Enter ammount of EUR","100"))
+	  {
+		  alert("Ops, try please again");
+	  }
+
+		  itemEuro = itemEuro.replace(/\s/g, '')
+		  itemEuro = itemEuro.replace(/\D/g,'')
+		  itemEuro = parseInt(itemEuro); 		
+
+		 
+
+ 	var itemDollar;
+	  itemDollar = prompt("Enter ammount of Dollar");
  	
-	for (itemDollar; itemDollar.length >= 8; itemDollar = prompt("Enter ammount of Dollar"))
+	for (itemDollar; itemDollar.length >= 10; itemDollar =       prompt("Enter ammount of Dollar"))
 	  {
 		alert("Ops, try please again");
 	  }
-		 itemDollar.trim() === "" || isNaN(itemDollar);
- 
+		
+  		  itemDollar = itemDollar.replace(/\s/g, '')
+		  itemDollar = itemDollar.replace(/\D/g,'')
+		  itemDollar = parseInt(itemDollar); 	
 
 
  var Exchange_EurToGrn = itemEuro * euro;
